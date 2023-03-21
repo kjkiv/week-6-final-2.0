fbf86565147979dfdceb948d9d8f4ef832dbcd23
Karl <kjkiv@yahoo.com>
2023-03-20 21:55:19 -0400

    push2

--- deck.js
+++ deck.js
@@ -15,7 +15,7 @@ const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"
         return this.cards.length
     }
 
-    pop(){
+    pop() { 
        return this.cards.shift() 
     }
 
